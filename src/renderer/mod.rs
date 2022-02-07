4c8d1d2d1acbc98c5b691bb8fff1fd134d587fef
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-02-07 20:38:21 +0100

    Drop the OpenGL feature
    Since this wouldn't work without it anyways

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,6 +1,3 @@
-#[cfg(feature = "opengl")]
 mod opengl_renderer;
-#[cfg(feature = "opengl")]
 pub use opengl_renderer::RenderSettings;
-#[cfg(feature = "opengl")]
 pub(crate) use opengl_renderer::Renderer;
